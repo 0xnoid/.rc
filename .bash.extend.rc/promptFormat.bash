ab22cbc0ef3cb31c134483c1a4a5262d736851e4
Eivind <10182309+0xnoid@users.noreply.github.com>
2026-01-27 14:34:06 +0100

    Create promptFormat.bash

---
+++ .bash.extend.rc/promptFormat.bash
@@ -0,0 +1,3 @@
+# Change prompt style
+## test|hostie[mail}~* < {username}|{prettyHostName}[{dir}]~*
+export PS1="\\u\\[\\e[37m\\]|\\[\\e[m\\]\\h\\[\\e[37m\\][\\[\\e[m\\]\\W\\[\\e[37m\\]]~\\[\\e[m\\]\\[\\e[31m\\]*\\[\\e[m\\] "
